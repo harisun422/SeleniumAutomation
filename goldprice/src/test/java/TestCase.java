449794f356cb341824810d84eabc528416b29ac0
hari <hari@hari-H110>
2024-01-04 01:26:51 +0530

    test will fetch gold rate for today

--- goldprice/src/test/java/TestCase.java
+++ goldprice/src/test/java/TestCase.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
-
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import goldprice.Goldprice;
 
 public class TestCase {
@@ -10,7 +11,11 @@ public class TestCase {
 		
 		//format - 04/December/2023
 		
-		Double value = gp.table("12/December/2023");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MMMM/YYYY");
+		Date date = new Date();  
+		String today_date =(dateFormat.format(date));
+		System.out.println("today date: "+today_date);
+		Double value = gp.table(today_date);
 		if(value < 1){
 			System.out.println("failed");
 		}else {
